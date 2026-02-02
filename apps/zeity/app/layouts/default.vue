80f7f5970aff06921c442f105f83ab769ad81691
Alexander Friesen <friesen.alex.home@gmail.com>
2026-02-02 13:05:39 +0000

    fix: correct viewport height calculation in layout

--- apps/zeity/app/layouts/default.vue
+++ apps/zeity/app/layouts/default.vue
@@ -3,7 +3,7 @@
         <AppHeader />
 
         <UContainer>
-            <div class="grid md:flex md:gap-4 min-h-[calc(100vh-var(--ui-header-height))] divide-x divide-default">
+            <div class="grid md:flex md:gap-4 min-h-[calc(100dvh-var(--ui-header-height))] divide-x divide-default">
                 <aside
                     class="hidden md:block flex-shrink-0 sticky top-(--ui-header-height) max-h-[calc(100vh-var(--ui-header-height))] md:w-44 lg:w-52 xl:w-60 overflow-y-auto">
                     <AppAsideNavigation />
