3ae9ed4b8e4e4436463b2c67dd0e7e0809fca218
Alexander Friesen <friesen.alex.home@gmail.com>
2026-02-17 18:32:52 +0000

    another try

--- apps/zeity/app/components/app/Hints.client.vue
+++ apps/zeity/app/components/app/Hints.client.vue
@@ -1,76 +1,93 @@
 <script setup lang="ts">
+const pwa = useNuxtApp().$pwa;
+const { t } = useI18n();
 const { loggedIn } = useUserSession();
 
-const closedHints = ref({
-  auth: false,
-  pwaInstall: false,
-  pwaRefresh: false,
+const closedHints = ref<string[]>([]);
+
+const availableHints = computed(() => {
+  const result = [];
+
+  if (!loggedIn.value) {
+    result.push({
+      id: 'auth-hint',
+      color: 'neutral' as const,
+      icon: 'i-lucide-lock',
+      title: t('hints.auth.title'),
+      description: t('hints.auth.description'),
+      actions: [
+        {
+          icon: 'i-lucide-log-in',
+          label: t('hints.auth.login'),
+          to: '/auth',
+        },
+      ],
+    });
+  }
+
+  if (pwa?.showInstallPrompt && !pwa?.needRefresh) {
+    result.push({
+      id: 'pwa-install-hint',
+      color: 'neutral' as const,
+      icon: 'i-lucide-smartphone',
+      title: t('hints.pwaInstall.title'),
+      description: t('hints.pwaInstall.description'),
+      actions: [
+        {
+          icon: 'i-lucide-download',
+          label: t('hints.pwaInstall.install'),
+          onClick: () => pwa?.install(),
+        },
+      ],
+    });
+  }
+
+  if (pwa?.needRefresh) {
+    result.push({
+      id: 'pwa-refresh-hint',
+      color: 'neutral' as const,
+      icon: 'i-lucide-smartphone',
+      title: t('hints.pwaRefresh.title'),
+      description: t('hints.pwaRefresh.description'),
+      actions: [
+        {
+          icon: 'i-lucide-refresh-cw',
+          label: t('hints.pwaRefresh.refresh'),
+          onClick: () => pwa?.updateServiceWorker(),
+        },
+      ],
+    });
+  }
+
+  return result;
 });
+
+const hints = computed(() =>
+  availableHints.value.filter((hint) => !closedHints.value.includes(hint.id)),
+);
+
+const hint = computed(() =>
+  availableHints.value
+    .filter((hint) => !closedHints.value.includes(hint.id))
+    .at(0),
+);
+
+function closeHint() {
+  closedHints.value.push(hint.value?.id || '');
+}
 </script>
 
 <template>
-  <div class="relative flex w-full snap-x snap-mandatory gap-6 overflow-x-auto">
-    <UAlert
-      v-if="!closedHints.auth && !loggedIn"
-      :title="$t('hints.auth.title')"
-      :description="$t('hints.auth.description')"
-      :actions="[
-        {
-          icon: 'i-lucide-log-in',
-          label: $t('hints.auth.login'),
-          to: '/auth',
-        },
-      ]"
-      icon="i-lucide-lock"
-      color="neutral"
-      variant="outline"
-      class="shrink-0 snap-center"
-      close
-      @update:open="closedHints.auth = true"
-    />
-
-    <UAlert
-      v-if="
-        !closedHints.pwaInstall && $pwa?.showInstallPrompt && !$pwa?.needRefresh
-      "
-      :title="$t('hints.pwaInstall.title')"
-      :description="$t('hints.pwaInstall.description')"
-      :actions="[
-        {
-          icon: 'i-lucide-download',
-          label: $t('hints.pwaInstall.install'),
-          onClick: () => {
-            $pwa?.install();
-          },
-        },
-      ]"
-      icon="i-lucide-smartphone"
-      color="neutral"
-      variant="outline"
-      class="shrink-0 snap-center"
-      close
-      @update:open="closedHints.pwaInstall = true"
-    />
-
-    <UAlert
-      v-if="!closedHints.pwaRefresh && $pwa?.needRefresh"
-      :title="$t('hints.pwaRefresh.title')"
-      :description="$t('hints.pwaRefresh.description')"
-      :actions="[
-        {
-          icon: 'i-lucide-refresh-cw',
-          label: $t('hints.pwaRefresh.refresh'),
-          onClick: () => {
-            $pwa?.updateServiceWorker();
-          },
-        },
-      ]"
-      icon="i-lucide-smartphone"
-      color="neutral"
-      variant="outline"
-      class="snap-center"
-      close
-      @update:open="closedHints.pwaRefresh = true"
-    />
-  </div>
+  <UAlert
+    v-for="hint in hints"
+    :key="hint.id"
+    :icon="hint.icon"
+    :color="hint.color"
+    :title="hint.title"
+    :description="hint.description"
+    :actions="hint.actions"
+    variant="outline"
+    close
+    @update:open="(value) => !value && closeHint()"
+  />
 </template>
