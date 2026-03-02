aaf5559f55d1834c2e2fb83a8e82015437bde62a
Alexander Friesen <friesen.alex.home@gmail.com>
2026-03-02 12:11:33 +0000

    style(navigation): simplify template structure and improve class bindings

--- apps/zeity/app/components/app/BottomNavigation.vue
+++ apps/zeity/app/components/app/BottomNavigation.vue
@@ -13,22 +13,11 @@ watch(
 </script>
 
 <template>
-  <div
-    class="fixed bottom-0 inset-x-0 flex justify-center pointer-events-none"
-    :class="$attrs.class"
-  >
-    <div
-      class="pointer-events-auto rounded-t-xl bg-(--ui-bg) border border-(--ui-border) shadow-md"
-    >
+  <div class="fixed bottom-0 inset-x-0 flex justify-center pointer-events-none">
+    <div class="pointer-events-auto rounded-t-xl bg-default border border-default shadow-md">
       <nav class="relative flex items-center justify-center">
-        <ul
-          class="isolate min-w-0 flex items-center justify-between gap-2 px-3"
-        >
-          <li
-            v-for="button in horizontalMenu"
-            :key="button.label"
-            class="relative min-w-18 py-1"
-          >
+        <ul class="isolate min-w-0 flex items-center justify-between gap-2 px-3">
+          <li v-for="button in horizontalMenu" :key="button.label" class="relative min-w-18 py-1">
             <ULink
               class="group relative w-full flex flex-1 flex-col items-center gap-0.5 px-2.5 py-1.5 font-medium text-sm before:absolute before:z-[-1] before:rounded-md focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:before:ring-primary before:inset-x-px before:inset-y-0"
               active-class="before:bg-elevated before:bg-elevated"
