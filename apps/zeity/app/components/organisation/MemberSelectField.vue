96b7e7c6631eb1bfaac6a2418243735438bbb953
Alexander Friesen <friesen.alex.home@gmail.com>
2026-03-10 08:09:32 +0000

    perf(organisations): improve reactivity in member select field

--- apps/zeity/app/components/organisation/MemberSelectField.vue
+++ apps/zeity/app/components/organisation/MemberSelectField.vue
@@ -1,58 +1,61 @@
 <script setup lang="ts">
-import { useDebounce } from '@vueuse/core';
+import { refDebounced } from '@vueuse/core';
 
 const model = defineModel<string[]>();
 const { organisationId, excludeTeam } = defineProps({
-    organisationId: {
-        type: String,
-        required: true,
-    },
-    excludeTeam: {
-        type: String,
-        required: false,
-        default: undefined,
-    },
+  organisationId: {
+    type: String,
+    required: true,
+  },
+  excludeTeam: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
 });
 
-const query = ref('')
-const queryDebounced = useDebounce(query, 300);
-const { data, pending, refresh } = await useLazyAsyncData(
-    'search-members',
-    () => $fetch(
-        `/api/organisation/${organisationId}/member`,
-        {
-            query: {
-                excludeTeam,
-                search: queryDebounced.value,
-            },
-        }
-    ),
-    {
-        server: false,
-        immediate: false,
-        watch: [queryDebounced],
-    }
+const query = shallowRef('');
+const queryDebounced = refDebounced(query, 300);
+const queryParams = computed(() => ({
+  excludeTeam,
+  search: queryDebounced.value,
+}));
+const { data, pending, refresh } = await useLazyFetch(
+  () => `/api/organisation/${organisationId}/member`,
+  {
+    query: queryParams,
+    server: false,
+    immediate: false,
+  },
 );
 
 function openChange(value: boolean) {
-    if (value) {
-        // trigger refresh on select open
-        refresh();
-    }
+  if (value) {
+    // trigger refresh on select open
+    refresh();
+  }
 }
 </script>
 
-
 <template>
-    <UInputMenu v-model="model" v-model:search-term="query" :loading="pending" :items="data?.items" multiple size="xl"
-        value-key="id" label-key="user.name" @update:open="openChange">
-        <template #item="{ item }">
-            <div class="flex items-center gap-3">
-                <UAvatar :src="getUserImagePath(item.user)" :alt="`${item.user?.name}`" />
-                <p class="font-medium text-highlighted">
-                    {{ item.user?.name }}
-                </p>
-            </div>
-        </template>
-    </UInputMenu>
+  <UInputMenu
+    v-model="model"
+    v-model:search-term="query"
+    :loading="pending"
+    :items="data?.items"
+    multiple
+    size="xl"
+    value-key="id"
+    label-key="user.name"
+    @update:open="openChange"
+  >
+    <template #item="{ item }">
+      <div class="flex items-center gap-3">
+        <UAvatar :src="getUserImagePath(item.user)" :alt="`${item.user?.name}`" />
+        <p class="font-medium text-highlighted">
+          {{ item.user?.name || $t('user.deleted') }}
+        </p>
+      </div>
+    </template>
+  </UInputMenu>
 </template>
