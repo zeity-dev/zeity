9d8a8e5233365db8c9d98ee406ec610aa56d56c9
Alexander Friesen <friesen.alex.home@gmail.com>
2026-07-22 14:11:20 +0000

    feat(ci): upload code coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     projects: ['./apps/*/vitest.config.ts', './libs/*/vitest.config.ts'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'cobertura'],
+    },
   },
 });
